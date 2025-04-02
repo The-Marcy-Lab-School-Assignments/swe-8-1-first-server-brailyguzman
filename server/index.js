235bb277ca445433f291046947dd0c295444647c
Braily Guzman <brailyg@outlook.com>
2025-04-01 23:15:10 -0400

    Add current progress

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+const logRoutes = (req, res, next) => {
+  console.log(`${req.method}: ${req.url} - ${new Date().toLocaleTimeString()}`);
+
+  next();
+};
+
+const jokeController = (_req, res) => {
+  res.json({
+    joke: "Why don't skeletons fight each other? Because they don't have the guts!",
+  });
+};
+
+const pictureController = (_req, res) => {
+  res.json({
+    picture:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Cima_da_Conegliano%2C_God_the_Father.jpg/300px-Cima_da_Conegliano%2C_God_the_Father.jpg',
+  });
+};
+
+const rollDieController = (_req, res) => {
+  const roll = Math.floor(Math.random() * 6) + 1;
+
+  res.json([roll]);
+};
+
+// Endpoints
+router.get('/joke', jokeController);
+router.get('/picture', pictureController);
+router.get('/rollDie', rollDieController);
+
+// Use the logRoutes middleware for all endpoints
+app.use(logRoutes);
+// Use the router on the /api route
+app.use('/api', router);
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
